64ecdc6d1fe33f12a291e4f62251c6ed59ae86c3
izvest <eholsti@gmail.com>
2017-04-22 18:50:04 +0300

    dft

---
+++ fourier/fourier.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class fourier{
+ public static void main(String[] Args){
+  Scanner sc = new Scanner(system.in);
+  System.out.print("Input: ");
+  String in = sc.nextLine();
+  sc.close();
+ }
+
+ public static int FTT(int s){
+
+ }
+
+ public static int DTF(int s){
+  String out = "";
+  int n = ((String)s.toString()).length();
+  for (int k : s){
+   int sum = 0;
+   for (int g : s){
+    sum += 
+   }
+   int fx = (1/n)*sum;
+  }
+ }
+}
