a5004b8dd740bf342dc4e59db5f52517f26d11ba
Emil Holsti <izvest@users.noreply.github.com>
2018-03-20 14:23:44 +0000

    planetfinder init

---
+++ planetfinder/Planet.java
@@ -0,0 +1,16 @@
+public class Planet {
+	
+	public String atmosphere;
+	public String temperature;
+	public String gravity;
+	public String water;
+	public String[] resources;
+	
+	public Planet() {
+		atmosphere = ;
+		temperature = ;
+		gravity = ;
+		water = ;
+		resources[] = ;
+	}
+}
---
+++ planetfinder/Spaceship.java
@@ -0,0 +1,31 @@
+public class Spaceship {
+	
+	public int fuel;
+	public int passengers;
+	
+	public int atmosphereScanner;
+	public int tempScanner;
+	public int gravityScanner;
+	public int waterScanner;
+	public int resourceScanner;
+	
+	public int scienceDatabase;
+	public int cultureDatabase;
+	
+	public int landingGear;
+	public int constructionGear;
+	
+	public Spaceship() {
+		fuel = 100;
+		passengers = 1000;
+		atmosphereScanner = 100;
+		tempScanner = 100;
+		gravityScanner = 100;
+		waterScanner = 100;
+		resourceScanner = 100;
+		scienceDatabase = 100;
+		cultureDatabase = 100;
+		landingGear = 100;
+		constructionGear = 100;
+	}	
+}
